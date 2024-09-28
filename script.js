fe5988c0e2774a1cdb7e5c39cc876577b0dbc4d2
Claus-Factor <volxin.k2004@gmail.com>
2024-09-28 21:51:59 +0300

    Almost ready

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector(".menu");
 const mobileNav = document.querySelector(".nav__mobile-list");
+const header = document.querySelector(".header");
 menu.onclick = () => {
     mobileNav.classList.toggle("none");
     document.body.classList.toggle("no-overflow");
+    header.classList.toggle("shadow");
 }
